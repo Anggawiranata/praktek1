638074ae64f0fc0c8d0389d7635444ecde0a4cb2
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-03-27 22:24:06 +0800

    Mengisi nilai attribut

--- praktek01/src/praktek01/printeraksi.java
+++ praktek01/src/praktek01/printeraksi.java
@@ -14,6 +14,25 @@ public class printeraksi {
         printer led = new printer();
         printer laserjet = new printer();
         printer plotter = new printer();
+        
+        led.merk="canon";
+        led.harga=650.000;
+        led.jenis="ink jet";
+        led.kualitas="baik";
+        led.type="ip2770";
+        
+       
+        laserjet.merk="epson";
+        laserjet.harga=1000.000;
+        laserjet.jenis="dot matrix";
+        laserjet.kualitas="sangat baik";
+        laserjet.type="epsonl30";
+        
+        plotter.merk="hp";
+        plotter.harga=1.500;
+        plotter.jenis="ink jet";
+        plotter.kualitas="baik";
+        plotter.type="hp230";
    }
       
 }
